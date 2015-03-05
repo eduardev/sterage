d0603178ac181d5aa0b71ef447a877c54ea34cb1
Edu <Edu@Jupiter.home>
2015-03-05 18:48:10 +0000

    Initial Commit

---
+++ loader.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * Sterage - a set of scripts/classes/files to help kickstart wordpress theme using Roots/Sage.
+ * All code under MIT license.
+ * @author Eduardo Pereira
+ * @url http://edu.ardo.pt
+ * @date 5/mar/2015 18:47:39
+ */
+
